3059a83c0de5401798ffa88a6ec538b0329ba300
David Roth <roth.david00@gmail.com>
2018-08-09 10:07:41 +0200

    -Fixed some problems -Removed unused stuff -Changed answer of question

--- getNumber.js
+++
@@ -1,8 +0,0 @@
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostEquals: 'game.energy.ch'}
-      })],
-      actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
-});
