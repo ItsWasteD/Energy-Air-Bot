f923eb808adbfabb19ec76cbba8f1f80017422c9
Dave <roth.david00@gmail.com>
2021-08-12 12:08:10 +0200

    Updated for the 2021 quiz.

--- NRJ.js
+++ NRJ.js
@@ -5,7 +5,7 @@ function eAirGame() {
     //document.head.parentNode.removeChild(document.head);
   }
 
-  var timeout = 500;
+  var timeout = 250;
 
   var questions = [
     {question: "Wie viele Energy Air Tickets werden verlost?", answer: "40’000"},
@@ -63,6 +63,44 @@ function eAirGame() {
     {question: "In welcher Hauptstadt Europas machen die BAD SPIES keinen Halt?", answer: "Wien"},
     {question: "Aus welchem Film ist Mila Kunis bekannt?", answer: "Bad Moms"}];
 
+  var questions2021 = [
+    {question: "WIE HEISST DER OFFIZIELLE INSTAGRAM-ACCOUNT DES ENERGY AIR?", answer: "@energyair_official"},
+    {question: "Musikgrössen aus wie vielen Ländern waren am Energy Air 2019 dabei?", answer: "Aus 7 Ländern"},
+    {question: "IN WELCHER LOCATION FINDET DAS ENERGY AIR 2021 UNTER FREIEM HIMMEL STATT?", answer: "Nationalstadion der Schweiz Wankdorf Bern"},
+    {question: "IN WELCHEM SCHWEIZER KANTON ERÖFFNETE TALLY WEIJL 1987 DEN ERSTEN STORE?", answer: "Fribourg"},
+    {question: "WAS FOLGT AM DIESJÄHRIGEN ENERGY AIR ALS KRÖNENDER ABSCHLUSS?", answer: "Aftershowparty"},
+    {question: "IN WIE VIELEN LÄNDERN IST DAS KLEIDERGESCHÄFT TALLY WEIJL VERTRETEN?", answer: "In 39 Ländern"},
+    {question: "WELCHE MUSIKERIN WURDE AM ENERGY AIR 2018 VON EINER 9-JÄHRIGE BESUCHERIN AUF DER BÜHNE GECOVERT?", answer: "Namika"},
+    {question: "WIE KANNST DU DEINE GEWINNCHANCEN BEI TICKETVERLOSUNGEN FÜR ENERGY EVENTS VERDOPPELN?", answer: "Mit einer Energy One Membership"},
+    {question: "WAS WAR DAS ERSTE, WAS KÜNSTLER KNACKEBOUL NACH SEINEM AUFTRITT 2014 BACKSTAGE GEMACHT HAT?", answer: "Mit seinem Mami ein kühles Bier getrunken"},
+    {question: "WELCHE STADT GEHÖRT SEIT AUGUST AUCH ZUR ENERGY FAMILIE UND WIRD AM ENERGY AIR VERTRETEN SEIN?", answer: "Luzern"},
+    {question: "IN WELCHEN FARBEN TRITT DAS ENERGY AIR LOGO JÄHRLICH FÜR DAS SOMMERFINALE AUF?", answer: "Blau und Weiss"},
+    {question: "NACH WELCHEM KRITERIUM WÄHLT DAS ENERGY TEAM DIE ACTS FÜR DAS ENERGY AIR AUS?", answer: "Musiker*innen, welche einen grossen Bekanntheitsgrad haben"},
+    {question: "UNTER WELCHEM MOTTO FEIERN WIR AM 4. SEPTEMBER 2021 DAS ENERGY AIR?", answer: "We are back."},
+    {question: "MIT WELCHEM ESC-HIT ROCKTE LUCA HÄNNI AM LETZTEN ENERGY AIR DIE BÜHNE?", answer: "She Got Me"},
+    {question: "WELCHER KÜNSTLER MUSSTE AM LETZTEN ENERGY AIR BACKSTAGE EINEN PART AUS DEM DIALEKTRAPSONG VON SANDRO VORRAPPEN?", answer: "Stress"},
+    {question: "MIT WELCHEM AUFBLASBAREN TIER KONNTEN ZWEI AUSERWÄHLTE AM LETZTEN ENERGY AIR ÜBER DIE GANZE MEUTE CROWDSURFEN?", answer: "Schwan"},
+    {question: "VON WELCHER MARKE WAR DAS MOTORRAD, MIT DEM LOCO ESCRITO AM LETZTEN ENERGY AIR ÜBER DIE BÜHNE FUHR?", answer: "Harley-Davidson"},
+    {question: "WELCHER ACT FEIERTE AM LETZTEN ENERGY AIR MIT EINEM NEUEN SONG EINE WELTPREMIERE?", answer: "Aloe Blacc"},
+    {question: "MIT WELCHER ZUSATZOPTION HAST DU DIE MÖGLICHKEIT, DIREKT VOR DER BÜHNE ZU STEHEN?", answer: "XTRA Circle"},
+    {question: "WELCHE ZWEI ENERGY KULTFIGUREN MISCHTEN DAS ENERGY AIR 2017 RICHTIG AUF?", answer: "Tinu & Dänu"},
+    {question: "WELCHEN KLEIDUNGSSTIL VERFOLGT TALLY WEIJL GRUNDSÄTZLICH?", answer: "Just in time (voll im Trend)"},
+    {question: "WANN IST DIE TICKETVERLOSUNG FÜRS ENERGY AIR 2021 GESTARTET?", answer: "Am 2. August 2021"},
+    {question: "WAS PASSIERT, WENN ES AM ENERGY AIR REGNET?", answer: "Der Event findet trotzdem statt"},
+    {question: "WAS IST DAS PERFEKTE OPENAIR-OUTFIT?", answer: "Egal, hauptsache du kannst darin tanzen"},
+    {question: "WIE ALT MUSS MAN SEIN, UM OHNE ERWACHSENE BEGLEITUNG AM ENERGY AIR TEILZUNEHMEN?", answer: "14 Jahre"},
+    {question: "WOMIT ERSCHIENEN DIE ENERGY MEIN MORGEN MODERATOREN MOSER UND SCHELKER AUF DER ENERGY AIR BÜHNE 2019?", answer: "Mit Spielzeug-Pferden"},
+    {question: "WIE WIRD TALLY WEIJL AUSGESPROCHEN?", answer: "Talli Weil"},
+    {question: "WIE LANGE DAUERTE DAS ENERGY AIR 2019?", answer: "5 1/2 Stunden"},
+    {question: "WER WAR DER ALLERERSTE ACT IN DER GESCHICHTE DES ENERGY AIR?", answer: "Bastian Baker"},
+    {question: "WELCHES SCHWEIZER DJ-DUO SORGTE AM ENERGY AIR 2019 ZU BEGINN FÜR REICHLICH STIMMUNG?", answer: "Averdeck"},
+    {question: "WO KANNST DU, UNTER ANDEREM, ENERGY AIR TICKETS GEWINNEN?", answer: "Am Sender bei Radio Energy"},
+    {question: "WELCHER ACT WAR NOCH NIE AN EINEM ENERGY AIR DABEI?", answer: "Michael Patrick Kelly"},
+    {question: "WER WAR DER ÜBERRASCHUNGSACT AM ENERGY AIR 2018?", answer: "Lo & Leduc"},
+    {question: "IN WELCHER BELIEBTEN SERIE WAR TALLY WEIJL ZU SEHEN?", answer: "Gossip Girl"},
+    {question: "WIE HEISST DIE AKTUELLE KAMPAGNE GEGEN HASS IM INTERNET, WELCHE SWISSCOM MIT ENERGY LANCIERT HAT?", answer: "Mute the hate"},
+    {question: "WIE HEISST DIE TRAM- UND BUSHALTESTELLE, WELCHE SICH DIREKT NEBEN DEM STADION WANKDORF BEFINDET?", answer: "Wankdorf Center"}]
+
     for(var i = 0; i < 10; i++) {
       setTimeout(answerQuestion, timeout);
       if(i == 9) {
@@ -71,7 +109,7 @@ function eAirGame() {
     }
 
     function finish() {
-      console.log("IN FINISH");
+
       var winPicture = document.getElementsByClassName("image")[0];
       if(winPicture != undefined) {
         winPicture.click();
@@ -91,11 +129,11 @@ function eAirGame() {
     }
 
     function answerQuestion() {
-      var qElem = document.getElementsByClassName('mobile-padding-question')[1];
+      var qElem = document.getElementsByClassName('question-text')[0];
       if(qElem != undefined) {
         var q = qElem.innerText;
         var answer = findAnswer(q);
-        console.log(q + " : " + answer);
+
         document.getElementById(answer).click();
         document.getElementById("next-question").click();
       }
@@ -103,8 +141,8 @@ function eAirGame() {
 
     function findAnswer(q) {
       var answer;
-      questions.forEach((c, i, a) => {
-        if(c.question == q) {
+      questions2021.forEach((c, i, a) => {
+        if(c.question.toLowerCase() == q.toLowerCase()) {
           answer = c.answer;
         }
       });
@@ -118,7 +156,7 @@ function eAirGame() {
 
     var title;
     if(btn != undefined) {
-      title = btn.parentElement.children[0].innerText;
+      title = btn.parentElement.children[1].innerText;
       if(title == "Leider verloren") {
         btn.click();
       }
@@ -129,5 +167,5 @@ function eAirGame() {
     }
   }
 
-  setInterval(eAirGame, 5000);
-  setInterval(checkFail, 5000);
+  setInterval(eAirGame, 2500);
+  setInterval(checkFail, 2500);
--- README.md
+++ README.md
@@ -1,14 +1,17 @@
 # Energy Air Bot
 This bot will do the Energy Air Quiz/Game for you really fast. It works but its not perfect as I created it in my freetime.
 
+# UPDATE 2021!!
+Hello guys, I took the time to rework the bot and add all the questions for the quiz of 2021. The process to start the game changed a bit.
+
 ## Getting Startet
 1. Open the Chrome browser enter [chrome://extensions](chrome://extensions).
 2. Press on "Load unpacked extension"/"Entpackte Erweiterung laden" and select the folder of the Energy Air bot.
-3. Go to [game.energy.ch](game.energy.ch) enter your phone number and then wait until you receive the pin on your phone.
-4. Enter the PIN press the button and the bot will automatically do his job.
+3. Go to [game.energy.ch](game.energy.ch) login with your account (it doesnt matter what kind of account)
+4. Go to the quiz page, start the quiz and let the magic happen.
 
 ## Help me!
-If you see any questions in the `questions` variable, that have a false answer please let me know! Or create a new Issue.
+If you see any questions in the `questions2021` variable, that have a false answer please let me know! Or create a new Issue.
 
 Thanks for your help :)
 
